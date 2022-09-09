0463db02040d798f3044f9459b7c7662a10602ed
alexandram <alexandram@vmware.com>
2022-09-09 18:44:48 -0400

    updated java app file

--- src/main/java/io/mycontact/web/MainController.java
+++ src/main/java/io/mycontact/web/MainController.java
@@ -1,19 +1,10 @@
 package io.mycontact.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class MainController {
-
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String  index() {
-        return "views/index";
-    }
-    @RequestMapping({"home", "/home.tpl"})
-    public ModelAndView home(){
-        return new ModelAndView("views/home");
-    }
+@SpringBootApplication
+public class MyContactApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyContactApplication.class, args);
+	}
 }
